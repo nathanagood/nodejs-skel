7c67a0b5a8abeb6a8eeda4dc7196b4428a96727a
Nathan Good <nathgood@amazon.com>
2018-07-05 11:35:37 -0500

    Added step to clean up files as CodeDeploy errors when files are already in place.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,4 +4,11 @@ basename=$(basename $0)
 
 echo "Running ${basename}..."
 
+# TODO: Can back these files up first if you perfer to do that.
+
+# CodeDeploy will error if there is already a file in place. Therefore,
+# before installing the files, the target should be cleaned up to
+# create a clean space in which to place all of the new files.
+rm -rf /var/www/*
+
 exit 0
