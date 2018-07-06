78ee84e6ce66b7ce3130b92251a51ea367f81f83
Nathan Good <nathgood@amazon.com>
2018-07-06 09:17:57 -0500

    Added code to before_install step to shut down node.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,6 +6,20 @@ echo "Running ${basename}..."
 
 # TODO: Can back these files up first if you perfer to do that.
 
+# Shut down node
+node_pid=`ps aux | grep \"node index.js\" | grep -v \"grep\" | awk -e \'{print $1}\'`
+
+kill $node_pid
+
+# The process by this point should have been killed, but we want to just
+# make sure.
+running_proccess=`ps aux | grep "node index.js" | grep -v "grep" | wc -l`
+
+if [ ${running_proccess} -ne 0 ]; then
+    echo "Was unable to stop node process..." 2> /var/log/before_install.log
+    exit 1
+fi
+
 # CodeDeploy will error if there is already a file in place. Therefore,
 # before installing the files, the target should be cleaned up to
 # create a clean space in which to place all of the new files.
