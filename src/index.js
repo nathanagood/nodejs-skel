08edbd171d0a61a20ba8643ef170afe1763e79bd
Nathan Good <nathgood@amazon.com>
2018-07-12 10:59:21 -0500

    Increased version number

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Test Version 4');
+  res.send('Test Version 5');
 });
 
 app.listen(3000, function () {
